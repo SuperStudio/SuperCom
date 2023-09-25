c9335143546dc5a6d3fa8bbe506ad7b13702dd56
SuperStudio <secret>
2023-09-25 13:28:22 +0800

    更新文件版本为 3.9

--- SuperCom/Properties/AssemblyInfo.cs
+++ SuperCom/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.9.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]

90aef82873131b0372220d94c36a1383c2c352f3
SuperStudio <secret>
2025-09-13 10:36:10 +0800

    新增基础接口、枚举

---
+++ SuperCom/Core/Events/EventType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCom.Core.Events
+{
+    public enum EventType
+    {
+        None,
+        OpenAll,
+        CloseAll,
+        ProcOne,
+        OpenTab,
+        Remark,
+        StatusChanged,
+        StatusText,
+        RemoveTabBar,
+        NotifyTabItemBaudRate,
+        ShowHighLight,
+        OnSendCommandModify,
+    }
+}
---
+++ SuperCom/Core/Interfaces/IViewTabItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCom.Core.Interfaces
+{
+
+    /// <summary>
+    /// 用户可见的选项卡
+    /// </summary>
+    public interface IViewTabItem
+    {
+        void Close();
+        void Open();
+    }
+}
